ef0c71b7708366392c2a2c1c272ec028fe650937
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-03 22:04:16 +0300

    gradle implementation add

--- app/src/main/java/com/example/RecipeClub/MainActivity.kt
+++ app/src/main/java/com/example/RecipeClub/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.RecipeClub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.tarifklub.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
