546067a1e9a20791da96d6da34268b8395d3d7c6
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-03 22:43:07 +0300

    binding definition

--- app/src/main/java/com/example/RecipeClub/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/RecipeClub/ui/list/ListFragment.kt
@@ -6,19 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.RecipeClub.R
+import com.example.RecipeClub.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
-
+    //first binding name
+    private lateinit var binding:FragmentListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding=FragmentListBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //View işlemleri burada gerçekleşir
     }
 
 }
