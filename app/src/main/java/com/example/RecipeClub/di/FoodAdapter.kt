a5023cbcabb46af8078402454d11f698fa533b43
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-05 22:22:42 +0300

    DiffCallBack compain object because public

--- app/src/main/java/com/example/RecipeClub/di/FoodAdapter.kt
+++ app/src/main/java/com/example/RecipeClub/di/FoodAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.RecipeClub.di
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.RecipeClub.data.network.FoodModel
+import com.example.RecipeClub.databinding.FoodItemDesignBinding
 
 class FoodAdapter:ListAdapter<FoodModel,FoodAdapter.FoodViewHolder>(DiffCallBack){
 
@@ -16,7 +18,21 @@ class FoodAdapter:ListAdapter<FoodModel,FoodAdapter.FoodViewHolder>(DiffCallBack
     override fun onBindViewHolder(holder:FoodAdapter.FoodViewHolder,position:Int){
 
     }
-    class FoodViewHolder(private val binding:):RecyclerView.ViewHolder(binding.root){
+    class FoodViewHolder(private val binding:FoodItemDesignBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(food:FoodModel){
+            binding.foodModel=food
+            binding.executePendingBindings()
+        }
+    }
+    //Static yapı-Her yerden ulaşılabilir yapı
+    companion object DiffCallBack:DiffUtil.ItemCallback<FoodModel>(){
+        override fun areItemsTheSame(oldItem: FoodModel, newItem: FoodModel): Boolean {
+            return oldItem.idCategory==newItem.idCategory
+        }
+
+        override fun areContentsTheSame(oldItem: FoodModel, newItem: FoodModel): Boolean {
+            return oldItem==newItem
+        }
 
     }
 }
