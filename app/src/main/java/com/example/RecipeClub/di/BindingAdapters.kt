4a922f9ce44ddb5a247d983fdab028b2a29d8e18
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-06 15:57:02 +0300

    BindingAdapters add Glide lib create

--- app/src/main/java/com/example/RecipeClub/di/BindingAdapters.kt
+++ app/src/main/java/com/example/RecipeClub/di/BindingAdapters.kt
@@ -1,11 +1,42 @@
 package com.example.RecipeClub.di
 
+import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.RecipeClub.R
 import com.example.RecipeClub.data.network.FoodModel
+import com.example.RecipeClub.ui.list.RecipesApiStatus
 
 @BindingAdapter("listData")
 fun bindRecyclerView(recyclerView: RecyclerView,data:List<FoodModel>){
     val adapter=recyclerView.adapter as FoodAdapter
     adapter.submitList(data)
+}
+@BindingAdapter("foodApiStatus")
+fun bindStatus(statusImageView: ImageView, status:RecipesApiStatus?){
+    when(status){
+        RecipesApiStatus.LOADING->{
+            statusImageView.visibility= View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        RecipesApiStatus.DONE ->{
+            statusImageView.visibility= View.GONE
+        }
+        else->{
+            statusImageView.visibility=View.VISIBLE
+            statusImageView.setImageResource(R.drawable.connect_error)
+        }
+
+    }
+}
+@BindingAdapter("strCategoryThumb")
+fun bindImage(imageView: ImageView,strCategoryThumb:String?){
+    strCategoryThumb?.let {
+        Glide.with(imageView.context).load(strCategoryThumb).apply(
+            RequestOptions().placeholder(R.drawable.loading_animation).error(R.drawable.broken)
+            ).into(imageView)
+    }
 }
