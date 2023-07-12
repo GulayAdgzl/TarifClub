f90583e35d2dc44b31add911a9788ace20ebd755
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-12 16:00:00 +0300

    DetailViewModel for factoryclass because context or application add

--- app/src/main/java/com/example/recipe/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/recipe/ui/detail/DetailFragment.kt
@@ -6,19 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.recipe.R
+import com.example.recipe.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
+    private lateinit var binding:FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding= FragmentDetailBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner=this
+
+
     }
 }
---
+++ app/src/main/java/com/example/recipe/ui/detail/DetailViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.recipe.ui.detail
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.recipe.data.network.FoodModel
+
+class DetailViewModel (food:FoodModel,app:Application):AndroidViewModel(app){
+    private val _selectedFood=MutableLiveData<FoodModel>()
+    val selectedFood:LiveData<FoodModel>
+       get()=_selectedFood
+
+    init {
+        _selectedFood.value=food
+    }
+}
