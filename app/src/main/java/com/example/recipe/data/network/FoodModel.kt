527cd0dd77a4175724ce5855bf85d68778ed4e28
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-12 16:39:42 +0300

    FoodModel @Parcelize add

--- app/src/main/java/com/example/recipe/data/network/FoodModel.kt
+++ app/src/main/java/com/example/recipe/data/network/FoodModel.kt
@@ -1,10 +1,14 @@
 package com.example.recipe.data.network
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+import java.io.Serializable
 
+@Parcelize
 data class FoodModel (
         val id:Int,
         val name:String,
         @Json (name = "image_url") val imageUrl:String,
         val house:String
-)
+):Parcelable
