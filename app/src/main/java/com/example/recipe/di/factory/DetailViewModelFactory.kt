e02813a16d98213bdffb8e2c71e866bf71b3acf6
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-12 16:18:32 +0300

    DetailViewModelFactory create

---
+++ app/src/main/java/com/example/recipe/di/factory/DetailViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.recipe.di.factory
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.recipe.data.network.FoodModel
+import com.example.recipe.ui.detail.DetailViewModel
+import java.lang.IllegalArgumentException
+
+class DetailViewModelFactory(private val foodModel: FoodModel,private val application: Application):ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailViewModel::class.java))
+            return DetailViewModel(foodModel,application) as T
+        throw IllegalArgumentException("Unknown ViewModel class ")
+    }
+}
